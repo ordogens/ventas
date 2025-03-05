eb95628f1bcbb229b259ccdff9d30fcd8f3258ab
Ordogen <oarcilagarcia@gmail.com>
2025-03-04 23:33:11 -0500

    integracion de la ligica

--- src/interfaz/Interfaz.jsx
+++ src/interfaz/Interfaz.jsx
@@ -1,20 +1,46 @@
 import "./Interfaz.css";
+import { useState } from "react";
 
 export const Interfaz = () => {
+  const [imput, setImput] = useState("");
+  const [boton, setBoton] = useState(0);
+  const [ventaMensual, setVentaMensual] = useState(0);
+
+  const suma = () => {
+    const nuevoValor = imput;
+    setBoton(boton + nuevoValor);
+    setVentaMensual(ventaMensual + nuevoValor);
+  };
+
   return (
     <div className="Interfaz">
       <div className="Interfas">
         <div className="valores">
           <h2>Total sales today:</h2>
+          <h2>${boton}</h2>
         </div>
         <div className="valores">
           <h2>Total sales this month:</h2>
+          <h2>${ventaMensual}</h2>
+          <h2>seus ganhos:</h2>
+          <h2>${ventaMensual * 0.06}</h2>
         </div>
       </div>
-        <div className="Input">
-        <label>Ingrese un valor:</label>
-        <input type="number" />
-        </div>
+      <div className="Input">
+        <label>Enter a value:</label>
+        <input
+          type="text"
+          value={imput}
+          onChange={(e) => setImput(Number(e.target.value) || 0)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              suma();
+              setImput(0)
+            }
+          }}
+        />
+        <button onClick={suma}>Enviar</button>
+      </div>
     </div>
   );
 };
