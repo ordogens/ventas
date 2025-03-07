cfa1558300cc7b41d9808e9bc9fa9945f52f3c99
Ordogen <oarcilagarcia@gmail.com>
2025-03-06 19:13:59 -0500

    pequeños cambios

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,16 @@
-import './Header.css'
+import "./Header.css";
 
 export const Header = () => {
   return (
-    <div className='Header'>
-        <h1>Sales Tracker</h1>
+    <div className="Header">
+      <h1>Sales Tracker</h1>
+      <nav>
+        <ul>
+         <li>Home</li>
+         <li>Abaut</li>
+         <li>Contact</li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
